75fd4bde9f8b9baaf2f5d4ebfd3676aec3fa69ba
Ugo <nuchukwuma1@gmail.com>
2026-09-08 21:46:04 +0100

    host

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ export default defineConfig({
   plugins: [react()],
   base: "/divic-frontend/",
   build: { outDir: "dist" },
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+    allowedHosts: ["divic-frontend.onrender.com"],
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["divic-frontend.onrender.com"],
+  },
 });
