cb5f5049e084c75fae87f05f5bb5bb5fe743b704
Ugo <nuchukwuma1@gmail.com>
2026-09-09 06:40:22 +0100

    config2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/divic-frontend/",
+  base: "/",
   build: { outDir: "dist" },
   server: {
     host: "0.0.0.0",
